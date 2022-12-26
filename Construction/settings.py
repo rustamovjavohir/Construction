6bc034245f90f9e8f61d72c7c9ab4f2f4d20da22
rustamovjavohir <rustamovj366@gmail.com>
2022-12-26 13:51:48 +0500

    settings

--- Construction/settings.py
+++ Construction/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-ktjcqow(b9iz%-g6a3gn)7+$7if4f(-()90wx^$^b2a#qv8x%y
 DEBUG = env.bool("DEBUG", default=False)
 # DEBUG = True
 # ALLOWED_HOSTS = json.loads(os.environ['ALLOWED_HOSTS'])
-ALLOWED_HOSTS='["*"]'
+ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
