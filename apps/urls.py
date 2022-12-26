72567e09494ed12c78269706b49a6c168a07352e
rustamovjavohir <rustamovj366@gmail.com>
2022-12-26 17:24:37 +0500

    merge main

--- apps/urls.py
+++ apps/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path, include
 
-
 urlpatterns = [
     path('advertsing/', include('apps.advertising.urls')),
     path('apartment/', include('apps.apartment.urls')),
     path('send-email/', include('apps.sendEmail.urls')),
 ]
+
+#  master branch
+
+# test branch
+
+# main branch
