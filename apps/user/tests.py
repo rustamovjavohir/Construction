daaf169e445b4093338c656e2f3f8bb194251514
rustamovjavohir <rustamovj366@gmail.com>
2023-03-26 14:30:55 +0500

    Create test

--- apps/user/tests.py
+++ apps/user/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from apps.user.models import User
+
 
 # Create your tests here.
+class TestUser(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(
+            name=" test",
+            email=" ",  # email is required
+            phone="998999999999",
+            is_web=True,
+            is_telegram=False,
+        )
+
+    def test_user(self):
+        self.assertEqual(self.user.name, " test")
+        self.assertEqual(self.user.email, " ")
+        self.assertEqual(self.user.phone, "998999999999")
+        self.assertEqual(self.user.is_web, True)
+        self.assertEqual(self.user.is_telegram, False)
