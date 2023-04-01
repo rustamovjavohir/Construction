34b99844888576287c0e7f20cc13e041f33ae09c
rustamovjavohir <rustamovj366@gmail.com>
2023-04-01 22:40:56 +0500

    add jazzmin admin panel

--- config/settings.py
+++ config/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = json.loads(os.environ['ALLOWED_HOSTS'])
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -238,6 +239,160 @@ SIMPLE_JWT = {
 }
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+JAZZMIN_SETTINGS = {
+    # title of the window (Will default to current_admin_site.site_title if absent or None)
+    "site_title": "Construction Admin",
+    #
+    # # Title on the login screen (19 chars max) (defaults to current_admin_site.site_header if absent or None)
+    "site_header": "Admin",
+    #
+    # # Title on the brand (19 chars max) (defaults to current_admin_site.site_header if absent or None)
+    "site_brand": "Construction",
+    #
+    # # Logo to use for your site, must be present in static files, used for brand on top left
+    # "site_logo": r'static\logo\logo1.png',
+
+    # # Logo to use for your site, must be present in static files, used for login form logo (defaults to site_logo)
+    # "login_logo": r'logo\logo.png',
+    #
+    # # Logo to use for login form in dark themes (defaults to login_logo)
+    # "login_logo_dark": None,
+    #
+    # # CSS classes that are applied to the logo above
+    # "site_logo_classes": "img-circle",
+    #
+    # # Relative path to a favicon for your site, will default to site_logo if absent (ideally 32x32 px)
+    "site_icon": None,
+    #
+    # # Welcome text on the login screen
+    "welcome_sign": "Construction Admin panel",
+    #
+    # # Copyright on the footer
+    "copyright": "rustamovdev.uz",
+    #
+    # # The model admin to search from the search bar, search bar omitted if excluded
+    "search_model": "auth.User",
+    #
+    # # Field name on user model that contains avatar ImageField/URLField/Charfield or a callable that receives the user
+    "user_avatar": 'photo',
+    #
+    # ############
+    # # Top Menu #
+    # ############
+    #
+    # # Links to put along the top menu
+    "topmenu_links": [
+
+        # Url that gets reversed (Permissions can be added)
+        {"name": "Home", "url": "admin:index", "permissions": ["auth.view_user"]},
+
+        # external url that opens in a new window (Permissions can be added)
+        {"name": "Дашбоард", "url": "dashboard", "new_window": True},
+
+        # model admin to link to (Permissions checked against model)
+        {"model": "auth.User"},
+
+        # App with dropdown menu to all its models pages (Permissions checked against models)
+        {"app": "order"},
+        {"app": "shop"},
+    ],
+    #
+    # #############
+    # # User Menu #
+    # #############
+    #
+    # # Additional links to include in the user menu on the top right ("app" url type is not allowed)
+    # "usermenu_links": [
+    #     {"name": "Support", "url": "http:/", "new_window": True},
+    #     {"model": "auth.user", "icon": ''}
+    # ],
+    #
+    # #############
+    # # Side Menu #
+    # #############
+    #
+    # # Whether to display the side menu
+    # "show_sidebar": True,
+    #
+    # # Whether to aut expand the menu
+    # "navigation_expanded": True,
+    #
+    # # Hide these apps when generating side menu e.g (auth)
+    # "hide_apps": ['auth_user'],
+    #
+    # # Hide these models when generating side menu (e.g auth.user)
+    # "hide_models": [],
+    #
+    # # List of apps (and/or models) to base side menu ordering off of (does not need to contain all apps/models)
+    "order_with_respect_to": ["user", "apartment", "advertising", "sendEmail", "selenium"],
+    #
+    # # Custom links to append to app groups, keyed on app name
+    "custom_links": {
+        "order": [
+            {
+                "name": "Телеграм канал ",
+                "url": "https://t.me/+rHSr3iwZxV1kYTNiassdf",
+                "new_window": True,
+                "icon": "fas fa-comments",
+            },
+            {
+                "name": "Телеграм бот",
+                "url": "https://t.me/diskont_rassrasdfochkabsdasotasdfasf",
+                "new_window": True,
+                "icon": "fas fa-robot",
+            },
+
+        ]
+    },
+    # # for the full list of 5.13.0 free icon classes
+    "icons": {
+        "user.User": "fas fa-user",
+        "sendEmail.Email": "fas fa-envelope",  # "fas fa-envelope"
+        "apartment.Apartment": "fas fa-building",
+        "shop.shop": "fas fa-store",
+        "shop.orderGroup": "fas fa-shopping-basket",
+        "auth_user.CustomUser": "fas fa-users-cog",
+        "auth_user.Manager": "fas fa-user-tie",
+        "auth_user.Seller": "fas fa-user",
+        "auth_user.BlackListUser": "fas fa-user-slash",
+        "auth.Group": "fas fa-users",
+
+    },
+    # # Icons that are used when one is not manually specified
+    # "default_icon_parents": "fas fa-chevron-circle-right",
+    # "default_icon_children": "fas fa-circle",
+    #
+    # #################
+    # # Related Modal #
+    # #################
+    # # Use modals instead of popups
+    # "related_modal_active": False,
+    #
+    # #############
+    # # UI Tweaks #
+    # #############
+    # # Relative paths to custom CSS/JS scripts (must be present in static files)
+    # "custom_css": None,
+    # "custom_js": None,
+    # # Whether to show the UI customizer on the sidebar
+    # "show_ui_builder": False,
+    #
+    # ###############
+    # # Change view #
+    # ###############
+    # # Render out the change view as a single form, or in tabs, current options are
+    # - single
+    # - horizontal_tabs (default)
+    # # - vertical_tabs
+    # # - collapsible
+    # # - carousel
+    # "changeform_format": "horizontal_tabs",
+    # # override change forms on a per modeladmin basis
+    # "changeform_format_overrides": {"auth.user": "collapsible", "auth.group": "vertical_tabs"},
+    # # Add a language dropdown into the admin
+    # "language_chooser": True,
+}
 
 try:
     from .local_settings import *
