aec53d69b7452169b399450e4f215fb6b4574053
rustamovjavohir <rustamovj366@gmail.com>
2023-05-04 13:26:49 +0500

    add middlewares

--- config/middlewares.py
+++ config/middlewares.py
@@ -1,4 +1,6 @@
 from django.conf import settings
+from django.shortcuts import redirect
+from django.urls import reverse
 from telegram import Bot
 
 
@@ -10,13 +12,20 @@ class TelegramErrorMiddleware:
 
     def __call__(self, request, *args, **kwargs):
         # do something before the view is called
-        print('before view')
+        # print('before view')
         response = self.get_response(request, *args, **kwargs)
-        print('after view')
+        # print('after view')
         return response
 
-    def process_exception(self, request, exception):
-        print('exception')
-        message = f"{request.path}: {str(exception)}"
-        self.bot.send_message(chat_id=self.chat_id, text=message)
-        return None
+    # def process_exception(self, request, exception):
+    #     print('exception')
+    #     message = f"{request.path}: {str(exception)}"
+    #     # self.bot.send_message(chat_id=self.chat_id, text=message)
+    #     return None
+
+    # def process_view(self, request, view_func, view_args, view_kwargs):
+    #     if not request.user.is_anonymous:
+    #         self.bot.send_message(chat_id=self.chat_id, text=f"{request.user} entered: <strong>{request.path}</strong>",
+    #                               parse_mode='HTML')
+    #         return None
+    #     return None
