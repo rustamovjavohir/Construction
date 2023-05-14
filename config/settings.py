7ec212007280c7d782340499a8dba8b366c8123d
rustamovjavohir <rustamovj366@gmail.com>
2023-05-14 19:02:25 +0500

    change database

--- config/settings.py
+++ config/settings.py
@@ -115,25 +115,19 @@ AUTH_USER_MODEL = "auth_user.CustomUser"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 
-# DATABASES = {'default': env.dj_db_url('DATABASE_URL')}
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': os.environ['DB_NAME'],
-#         'USER': os.environ['DB_USER'],
-#         'PASSWORD': os.environ['DB_PASS'],
-#         'HOST': os.environ['DB_SERVICE'],
-#         'PORT': os.environ['DB_PORT']
-#     }
-# }
-
-BASE_DIR = Path(__file__).resolve().parent.parent
+DATABASES = {'default': env.dj_db_url('DATABASE_URL')}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASS'],
+        'HOST': os.environ['DB_SERVICE'],
+        'PORT': os.environ['DB_PORT']
     }
 }
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # cd config cat settings.py
 # Password validation
